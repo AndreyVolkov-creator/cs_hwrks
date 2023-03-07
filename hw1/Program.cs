b02e67990c6545620c4d9296c258b70fadecf68b
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-07 23:24:51 +0300

    Second task

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,19 +1,28 @@
 ﻿#region 1
 
-    int a, b;
+    // int a, b;
 
-    Console.WriteLine("Введите первое число");
-    a = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите второе число");
-    b = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите первое число");
+    // a = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите второе число");
+    // b = Convert.ToInt32(Console.ReadLine());
 
-    if (a > b) Console.WriteLine(a);
-    else Console.WriteLine(b);
+    // if (a > b) Console.WriteLine(a);
+    // else Console.WriteLine(b);
     
 #endregion
 
 #region 2
 
+    int a, b, c;
 
+    Console.WriteLine("Введите первое число");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите третье число");
+    c = Convert.ToInt32(Console.ReadLine());
     
+    Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+
 #endregion
