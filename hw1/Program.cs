edfbf9c4898f2b91cb6d68481ea51cab02c56e9e
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-07 23:45:20 +0300

    Third task

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -14,15 +14,36 @@
 
 #region 2
 
-    int a, b, c;
 
-    Console.WriteLine("Введите первое число");
-    a = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите второе число");
-    b = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите третье число");
-    c = Convert.ToInt32(Console.ReadLine());
+
+    // int a, b, c;
+
+    // Console.WriteLine("Введите первое число");
+    // a = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите второе число");
+    // b = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите третье число");
+    // c = Convert.ToInt32(Console.ReadLine());
     
-    Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+    // Console.WriteLine(Math.Max(a, Math.Max(b, c)));
 
+#endregion
+
+#region 3
+
+    int a;
+
+    Console.WriteLine("Введите ваше число");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    if ((a % 2)==0)
+    {
+        Console.WriteLine("число четное");
+           
+    }
+    else
+    {
+        Console.WriteLine("Число нечетное");
+    }
+    
 #endregion
