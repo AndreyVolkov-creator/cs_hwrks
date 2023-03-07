df9788d23783bcee7ff52b6aaa52b106c3c64754
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-07 23:06:15 +0300

    First task

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿#region 1
+
+    int a, b;
+
+    Console.WriteLine("Введите первое число");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    b = Convert.ToInt32(Console.ReadLine());
+
+    if (a > b) Console.WriteLine(a);
+    else Console.WriteLine(b);
+    
+#endregion
+
+#region 2
+
+
+    
+#endregion
