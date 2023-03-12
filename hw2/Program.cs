4626a3c3a4ab9327e73c303e21e1954496028187
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-12 15:38:46 +0300

    1 task

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region 1
+    
+    int num, result;
+
+    Console.WriteLine("Введите трехзначное число");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > 99 && num < 1000)
+        {
+            while (num >= 100) num /= 10;
+            result = num%10;
+            Console.WriteLine($"Вторая цифра вашего числа {result}");
+        }
+    else Console.WriteLine("Введено неверное число");
+
+#endregion
+
+#region 2
+    
+#endregion
+
+#region 3
+    
+#endregion
