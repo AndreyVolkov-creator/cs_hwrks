6fa69a7d41f23acf09dd594896d18b0ae2e17349
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-12 15:41:48 +0300

    2 task

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,22 +1,35 @@
 ﻿#region 1
     
-    int num, result;
+    // int num, result;
 
-    Console.WriteLine("Введите трехзначное число");
-    num = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите трехзначное число");
+    // num = Convert.ToInt32(Console.ReadLine());
 
-    if (num > 99 && num < 1000)
-        {
-            while (num >= 100) num /= 10;
-            result = num%10;
-            Console.WriteLine($"Вторая цифра вашего числа {result}");
-        }
-    else Console.WriteLine("Введено неверное число");
+    // if (num > 99 && num < 1000)
+    //     {
+    //         while (num >= 100) num /= 10;
+    //         result = num%10;
+    //         Console.WriteLine($"Вторая цифра вашего числа {result}");
+    //     }
+    // else Console.WriteLine("Введено неверное число");
 
 #endregion
 
 #region 2
     
+    int num, result;
+
+    Console.WriteLine("Введите ваше число");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > 99)
+        {
+            while (num >= 1000) num /= 10;
+            result = num%10;
+            Console.WriteLine($"Тертья цифра вашего числа {result}");
+        }
+    else Console.WriteLine("Введено неверное число");
+
 #endregion
 
 #region 3
