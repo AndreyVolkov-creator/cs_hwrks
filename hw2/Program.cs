d0c48254aac325e98b7a05319ec3e07017f1d9b7
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-12 15:52:13 +0300

    3 task

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -17,21 +17,34 @@
 
 #region 2
     
-    int num, result;
+    // int num, result;
 
-    Console.WriteLine("Введите ваше число");
-    num = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите ваше число");
+    // num = Convert.ToInt32(Console.ReadLine());
 
-    if (num > 99)
-        {
-            while (num >= 1000) num /= 10;
-            result = num%10;
-            Console.WriteLine($"Тертья цифра вашего числа {result}");
-        }
-    else Console.WriteLine("Введено неверное число");
+    // if (num > 99)
+    //     {
+    //         while (num >= 1000) num /= 10;
+    //         result = num%10;
+    //         Console.WriteLine($"Тертья цифра вашего числа {result}");
+    //     }
+    // else Console.WriteLine("Введено неверное число");
 
 #endregion
 
 #region 3
     
+    int num;
+
+    Console.WriteLine("Введите ваше число");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > 0 && num < 8)
+        if (num == 6 || num == 7)
+            {
+                Console.WriteLine("Вы указали выходной день");
+            }  
+        else Console.WriteLine("Вы указали будний день");       
+    else Console.WriteLine("Вы не правильно указали день недели");
+
 #endregion
