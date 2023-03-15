a3522375fcf953f69fd5edeff615a1895adf7c93
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-15 14:05:06 +0300

    3 task

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region 1
+    
+
+
+#endregion
+
+#region 2
+    
+
+
+#endregion
+
+#region 3
+    
+    int num;
+
+    Console.WriteLine("Введите ваше число");
+    num = Convert.ToInt32(Console.ReadLine());
+    cubeNum(num);
+
+
+
+    
+    void cubeNum(int num)
+    {
+        int i, result;
+        for(i=1; i<=num; i++)
+        {
+            result = i * i * i;
+            Console.WriteLine(result);
+        }
+
+    }
+
+#endregion
