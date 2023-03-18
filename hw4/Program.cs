e053a78b174daea55e31f3535a4269320b461263
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-18 12:27:37 +0300

    1 task

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region 1
+
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+    int num, degree;
+
+    Console.WriteLine("Введите число");
+    num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень числа");
+    degree = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(NumDegree(num, degree));
+
+
+
+
+    int NumDegree(int a, int b)
+    {
+        int i = 0;
+        int result = 1;
+        for (i = 1; i <= b; i++)
+            result *= a;
+
+        return result;
+    }
+    
+#endregion
+
+#region 2
+    
+#endregion
+
+#region 3
+    
+#endregion
