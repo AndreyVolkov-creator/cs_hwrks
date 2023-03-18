06aac55f27d10a18d0290b548cc067181db47c13
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-18 12:51:13 +0300

    3 task

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -34,29 +34,55 @@
 // 82 -> 10
 // 9012 -> 12
 
-    int num;
+    // int num;
 
-    Console.WriteLine("Введите число");
-    num = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine(NumSumm(num));
-
-
+    // Console.WriteLine("Введите число");
+    // num = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine(NumSumm(num));
 
 
-    int NumSumm(int a)
-    {
-        int result = 0;
-        while (a > 0)
-        {
-            result += a % 10;
-            a /= 10;
-        }
 
-        return result;
-    }
+
+    // int NumSumm(int a)
+    // {
+    //     int result = 0;
+    //     while (a > 0)
+    //     {
+    //         result += a % 10;
+    //         a /= 10;
+    //     }
+
+    //     return result;
+    // }
 
 #endregion
 
 #region 3
     
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+    Console.WriteLine("Сейчас я создам массив из 8 случайных эллементов");
+    int[] array = new int [8];
+    FillArray(array);
+    PrintArray(array);
+
+
+
+
+    void FillArray(int[] array)
+    {
+        int i = 0;
+        for (i = 0; i < array.Length; i++)
+            array[i] = new Random().Next(0, 100);
+    }
+
+    void PrintArray(int[] array)
+    {
+        int i = 0;
+        for (i = 0; i <= array.Length - 1; i++)
+            Console.Write($"{array[i]} ");
+    }
+
 #endregion
