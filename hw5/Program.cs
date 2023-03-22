c8c4ff2905837933b53255de36e15b97e97f9587
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-22 20:56:04 +0300

    2 task

--- hw5/Program.cs
+++ hw5/Program.cs
@@ -4,10 +4,51 @@
 // [345, 897, 568, 234] -> 2
 
 
+// int[] array = new int[4];
+// FillArray(array);
+// PrintArray(array);
+// Console.WriteLine($"Кол-во четных эллементов массива равно {NumP(array)}");
+
+
+
+
+// void FillArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(100, 1000);
+// }
+
+// void PrintArray(int[] array)
+// {
+//     foreach (int item in array)
+//     {
+//         Console.Write($"{item} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int NumP(int[] array)
+// {
+//     int result = 0;
+//     foreach (int item in array)
+//     {
+//         if(item % 2 == 0) result++;
+//     }
+//     return result;
+// }
+    
+#endregion
+
+#region 2
+
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
 int[] array = new int[4];
 FillArray(array);
 PrintArray(array);
-Console.WriteLine($"Кол-во четных эллементов массива равно {NumP(array)}");
+Console.WriteLine($"Сумма элементов на нечетных позициях равна {Summ(array)}");
 
 
 
@@ -15,7 +56,7 @@ Console.WriteLine($"Кол-во четных эллементов массива
 void FillArray(int[] array)
 {
     for(int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(100, 1000);
+        array[i] = new Random().Next(0, 21);
 }
 
 void PrintArray(int[] array)
@@ -27,19 +68,15 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-int NumP(int[] array)
+int Summ(int[] array)
 {
     int result = 0;
-    foreach (int item in array)
-    {
-        if(item % 2 == 0) result++;
-    }
+
+    for(int i = 1; i < array.Length; i += 2)
+        result += array[i];
+
     return result;
 }
-    
-#endregion
-
-#region 2
     
 #endregion
 
