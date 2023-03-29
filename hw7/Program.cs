bd1927515a7add014b294da1ee1a7d4e51231c1b
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-29 17:18:02 +0300

    Variant with choice

--- hw7/Program.cs
+++ hw7/Program.cs
@@ -1,127 +1,65 @@
-﻿#region 1
+﻿double RandomDouble(double minValue, double maxValue)
+{
+    Random random = new Random();
 
-// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+	return minValue + (maxValue - minValue) * random.NextDouble();
+}
 
-// Console.WriteLine("Введите кол-во строк массива");
-// int rows = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите кол-во столбцов массива");
-// int columns = Convert.ToInt32(Console.ReadLine());
+void FillArrayFirstTask(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = Math.Round(RandomDouble(-10, 11),1);
+}
 
-// double[,] array = new double [rows, columns];
-// FillArray(array);
-// PrintArray(array);
+void PrintArrayFirstTask(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j], 4} \t");
+        Console.WriteLine();
+    }
+}
 
-
-
-
-// double RandomDouble(double minValue, double maxValue)
-// {
-//     Random random = new Random();
-
-// 	return minValue + (maxValue - minValue) * random.NextDouble();
-// }
-
-// void FillArray(double[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//         for (int j = 0; j < array.GetLength(1); j++)
-//             array[i,j] = Math.Round(RandomDouble(-10, 11),1);
-// }
-
-// void PrintArray(double[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//             Console.Write($"{array[i,j], 4} \t");
-//         Console.WriteLine();
-//     }
-// }
-
-#endregion
-
-#region 2
-
-// Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 17 -> такого числа в массиве нет
-
-// Console.WriteLine("Массив будет заполнен числами от 0 до 9. Введите, какое число вы хотите найти?");
-// int findNum = Convert.ToInt32(Console.ReadLine());
-
-// int[,] array = new int [5,5];
-// FillArray(array);
-// PrintArray(array);
-// int[] userNumIndex = FindNumIndex(array, findNum);
-// Console.WriteLine($"Ваше число находится на {userNumIndex[0]} строке, в {userNumIndex[1]} столбце");
-
-
-
-// int[] FindNumIndex(int[,] array, int userValue)
-// {
-//     int rowResult = 0;
-//     int colResult = 0;
+int[] FindNumIndex(int[,] array, int userValue)
+{
+    int rowResult = 0;
+    int colResult = 0;
     
-//     for (int i = 0; i < array.GetLength(0); i++)
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             if (array[i,j] == userValue)
-//             {
-//                 rowResult = i;
-//                 colResult = j;
-//                 goto Found;
-//             }
-//             else if (userValue > 9) Console.WriteLine("Вы ввели число, которого нет в массиве");
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] == userValue)
+            {
+                rowResult = i;
+                colResult = j;
+                goto Found;
+            }
+            else if (userValue > 9) Console.WriteLine("Вы ввели число, которого нет в массиве");
 
-//         }
-//     Found:       
-//     int[] result = {rowResult+1, colResult+1};
-//     return result;
-// }
+        }
+    Found:       
+    int[] result = {rowResult+1, colResult+1};
+    return result;
+}
 
-// void FillArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//         for (int j = 0; j < array.GetLength(1); j++)
-//             array[i,j] = new Random().Next(0,10);
-// }
+void FillArraySecondtTask(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i,j] = new Random().Next(0,10);
+}
 
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//             Console.Write($"{array[i,j], 2} \t");
-//         Console.WriteLine();
-//     }
-// }
-
-#endregion
-
-#region 3
-
-// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
-double[,] array = new double [3,4];
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-FindAverage(array);
-
-
-
+void PrintArraySecondtTask(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i,j], 2} \t");
+        Console.WriteLine();
+    }
+}
 
 void FindAverage(double[,] array)
 {
@@ -138,14 +76,14 @@ void FindAverage(double[,] array)
     
 }
 
-void FillArray(double[,] array)
+void FillArrayThirdTask(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
             array[i,j] = new Random().Next(1,10);
 }
 
-void PrintArray(double[,] array)
+void PrintArrayThirdTask(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -155,4 +93,45 @@ void PrintArray(double[,] array)
     }
 }
 
-#endregion
+
+
+
+
+Console.WriteLine("Выберите, какую задачу вы хотите посмотреть, 1, 2 или 3?");
+string choice = String.Empty;
+choice = Console.ReadLine(); 
+
+if (choice == "1")
+{
+    Console.WriteLine("ЗАДАНИЕ: Задайте двумерный массив своими размерами, заполненный случайными вещественными числами.");
+    Console.WriteLine("Введите кол-во строк массива");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите кол-во столбцов массива");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    double[,] arrayFirstTask = new double [rows, columns];
+    FillArrayFirstTask(arrayFirstTask);
+    PrintArrayFirstTask(arrayFirstTask);
+}
+else if(choice == "2")
+{
+    Console.WriteLine("ЗАДАНИЕ: Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.");
+    Console.WriteLine("Массив будет заполнен числами от 0 до 9. Введите, какое число вы хотите найти?");
+    int findNum = Convert.ToInt32(Console.ReadLine());
+
+    int[,] arraySecondtTask = new int [5,5];
+    FillArraySecondtTask(arraySecondtTask);
+    PrintArraySecondtTask(arraySecondtTask);
+    int[] userNumIndex = FindNumIndex(arraySecondtTask, findNum);
+    Console.WriteLine($"Ваше число находится на {userNumIndex[0]} строке, в {userNumIndex[1]} столбце");
+}
+else if(choice == "3")
+{
+    Console.WriteLine("ЗАДАНИЕ: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.");
+    double[,] arrayThirdTask = new double [3,4];
+    FillArrayThirdTask(arrayThirdTask);
+    PrintArrayThirdTask(arrayThirdTask);
+    Console.WriteLine();
+    FindAverage(arrayThirdTask);
+}
+else Console.WriteLine("Вы ввели неверное число");
