c509e010637c9c2ddb44d16047502721b3411166
Andrey_Volkov <mikelett0_games@mail.ru>
2023-03-29 15:59:51 +0300

    2 task

--- hw7/Program.cs
+++ hw7/Program.cs
@@ -6,102 +6,105 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-Console.WriteLine("Введите кол-во строк массива");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите кол-во столбцов массива");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите кол-во строк массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите кол-во столбцов массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-double[,] array = new double [rows, columns];
+// double[,] array = new double [rows, columns];
+// FillArray(array);
+// PrintArray(array);
+
+
+
+
+// double RandomDouble(double minValue, double maxValue)
+// {
+//     Random random = new Random();
+
+// 	return minValue + (maxValue - minValue) * random.NextDouble();
+// }
+
+// void FillArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             array[i,j] = Math.Round(RandomDouble(-10, 11),1);
+// }
+
+// void PrintArray(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//             Console.Write($"{array[i,j], 4} \t");
+//         Console.WriteLine();
+//     }
+// }
+
+#endregion
+
+#region 2
+
+// Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Массив будет заполнен числами от 0 до 9. Введите, какое число вы хотите найти?");
+int findNum = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int [5,5];
 FillArray(array);
 PrintArray(array);
+int[] userNumIndex = FindNumIndex(array, findNum);
+Console.WriteLine($"Ваше число находится на {userNumIndex[0]} строке, в {userNumIndex[1]} столбце");
 
 
 
-
-double RandomDouble(double minValue, double maxValue)
+int[] FindNumIndex(int[,] array, int userValue)
 {
-    Random random = new Random();
+    int rowResult = 0;
+    int colResult = 0;
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] == userValue)
+            {
+                rowResult = i;
+                colResult = j;
+                goto Found;
+            }
+            else if (userValue > 9) Console.WriteLine("Вы ввели число, которого нет в массиве");
 
-	return minValue + (maxValue - minValue) * random.NextDouble();
+        }
+    Found:       
+    int[] result = {rowResult+1, colResult+1};
+    return result;
 }
 
-void FillArray(double[,] array)
+void FillArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
-            array[i,j] = Math.Round(RandomDouble(-10, 11),1);
+            array[i,j] = new Random().Next(0,10);
 }
 
-void PrintArray(double[,] array)
+void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i,j], 4} \t");
+            Console.Write($"{array[i,j], 2} \t");
         Console.WriteLine();
     }
 }
 
 #endregion
 
-#region 2
-    
-#endregion
-
 #region 3
-    
-#endregion
-
-#region 4
-
-// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 10 3
-// После сортировки
-// 1 2 3 4
-// 5 7 9 10
-
-// Console.WriteLine("Введите кол-во строк массива");
-// int rows = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите кол-во столбцов массива");
-// int columns = Convert.ToInt32(Console.ReadLine());
-
-// int[,] array = new int [rows, columns];
-// FillArray(array);
-// PrintArray(array);
-
-
-// void SortArrayIndex(int[,] array)
-// {
-//     int min = 0;
-//     for (int i = 0; i < array.GetLength(0); i++)
-//         for (int j = 0; j < array.GetLength(1); j++)
-//             if (array[i,j] < min) 
-//             {}
-
-//     foreach (var item in array)
-//     {
-//         if (array[i,j] < item) array[i,j] = item;
-
-//     }
-// }
-
-// void FillArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//         for (int j = 0; j < array.GetLength(1); j++)
-//             array[i,j] = new Random().Next(0, 21);
-// }
-
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//             Console.Write($"{array[i,j], 3} \t");
-//         Console.WriteLine();
-//     }
-// }
     
 #endregion
