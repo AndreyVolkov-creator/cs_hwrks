999da4cf2d53091bee95c16c0c8dd7f05e6d8252
Andrey_Volkov <mikelett0_games@mail.ru>
2023-04-02 22:13:50 +0300

    4 task

--- hw8/Program.cs
+++ hw8/Program.cs
@@ -124,49 +124,110 @@
 // 18 20
 // 15 18
 
-int[,] firstArray = new int[2,2];
-int[,] secondArray = new int[2,2];
-int[,] thirdArray = new int[2,2];
-FillArrayThirdTask(firstArray);
-PrintArrayThirdTask(firstArray);
-Console.WriteLine();
-FillArrayThirdTask(secondArray);
-PrintArrayThirdTask(secondArray);
-for (int i = 0; i < 2; i++)
+// int[,] firstArray = new int[2,2];
+// int[,] secondArray = new int[2,2];
+// int[,] thirdArray = new int[2,2];
+// FillArrayThirdTask(firstArray);
+// PrintArrayThirdTask(firstArray);
+// Console.WriteLine();
+// FillArrayThirdTask(secondArray);
+// PrintArrayThirdTask(secondArray);
+// for (int i = 0; i < 2; i++)
+// {
+//     for (int j = 0; j < 2; j++)
+//     {
+//         for (int k = 0; k < 2; k++)
+//         {
+//             thirdArray[i, j] = thirdArray[i, j] + (firstArray[i, k] * secondArray[k, j]);
+//         }
+//     }
+// }
+// Console.WriteLine();
+// PrintArrayThirdTask(thirdArray);
+
+
+
+
+// void FillArrayThirdTask(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PrintArrayThirdTask(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+//---------------------------------------------------------------------------------------
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int deep1 = 2;
+int deep2 = 2;
+int deep3 = 2;
+int countNums = 89;
+int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
+
+for (int i = 0; i < resultNums.GetLength(0); i++)
 {
-    for (int j = 0; j < 2; j++)
+    for (int j = 0; j < resultNums.GetLength(1); j++)
     {
-        for (int k = 0; k < 2; k++)
+        for (int k = 0; k < resultNums.GetLength(2); k++)
         {
-            thirdArray[i, j] = thirdArray[i, j] + (firstArray[i, k] * secondArray[k, j]);
+            Console.WriteLine($"{resultNums[i, j, k]} - [{i},{j},{k}]");
         }
+        Console.WriteLine();
     }
+    Console.WriteLine();
 }
-Console.WriteLine();
-PrintArrayThirdTask(thirdArray);
 
 
 
 
-void FillArrayThirdTask(int[,] array)
+int[,,] Create3DMassive(int size1, int size2, int size3)
 {
+    int[,,] array = new int[size1, size2, size3];
+    int[] values = new int[countNums];
+    int num = 10;
+    for (int i = 0; i < values.Length; i++)
+        values[i] = num
+        ++;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        int randomInd = new Random().Next(0, values.Length);
+        int temp = values[i];
+        values[i] = values[randomInd];
+        values[randomInd] = temp;
+    }
+
+    int valueIndex = 0;
+
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(1, 10);
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = values[valueIndex++];
+            }
         }
     }
-}
-
-void PrintArrayThirdTask(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i,j]} ");
-        }
-        Console.WriteLine("");
-    }
+    return array;
 }
