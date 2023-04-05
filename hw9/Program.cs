689cb293a3beebedad736f21bc213ca0f234e26b
Andrey_Volkov <mikelett0_games@mail.ru>
2023-04-05 21:49:59 +0300

    2 task

--- hw9/Program.cs
+++ hw9/Program.cs
@@ -22,3 +22,20 @@
 // Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число");
+int minBorder = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int maxBorder = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+int SummRec(int num1, int num2)
+{
+    int sum = 0;
+    if(num1>num2) return sum;
+    return sum += num1 + SummRec(num1+1, num2);
+}
+
+Console.WriteLine(SummRec(minBorder, maxBorder));
