31015334400a86e3d85936800cc44c62c7cdeeab
Andrey_Volkov <mikelett0_games@mail.ru>
2023-04-05 21:44:08 +0300

    1 task

--- hw9/Program.cs
+++ hw9/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+// int NumListRec(int num)
+// {
+//     if (num==0) return 0;
+//     Console.Write($"{num} ");
+//     return NumListRec(num - 1);
+// }
+
+// NumListRec(num);
+
+//-----------------------------------------------------------------------
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
