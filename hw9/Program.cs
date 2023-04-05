fade86ffbd9d1441afa8ddbe71a598bb0260bf15
Andrey_Volkov <mikelett0_games@mail.ru>
2023-04-05 22:05:25 +0300

    Added user choice function

--- hw9/Program.cs
+++ hw9/Program.cs
@@ -1,58 +1,16 @@
-﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+﻿int NumListRec(int num)
+{
+    if (num==0) return 0;
+    Console.Write($"{num} ");
+    return NumListRec(num - 1);
+}
 
-// Console.WriteLine("Введите число");
-// int num = Convert.ToInt32(Console.ReadLine());
-
-
-
-
-// int NumListRec(int num)
-// {
-//     if (num==0) return 0;
-//     Console.Write($"{num} ");
-//     return NumListRec(num - 1);
-// }
-
-// NumListRec(num);
-
-//-----------------------------------------------------------------------
-
-// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
-
-// Console.WriteLine("Введите первое число");
-// int minBorder = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int maxBorder = Convert.ToInt32(Console.ReadLine());
-
-
-
-
-// int SummRec(int num1, int num2)
-// {
-//     int sum = 0;
-//     if(num1>num2) return sum;
-//     return sum += num1 + SummRec(num1+1, num2);
-// }
-
-// Console.WriteLine(SummRec(minBorder, maxBorder));
-
-//--------------------------------------------------------------------------------------
-
-// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
-
-Console.WriteLine("Введите первое число");
-int firstNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int secondNum = Convert.ToInt32(Console.ReadLine());
-
-
-
+int SummRec(int num1, int num2)
+{
+    int sum = 0;
+    if(num1>num2) return sum;
+    return sum += num1 + SummRec(num1+1, num2);
+}
 
 int AccermanFunc(int num1, int num2)
 {
@@ -61,4 +19,36 @@ int AccermanFunc(int num1, int num2)
     else return AccermanFunc(num1 - 1, AccermanFunc(num1, num2 - 1));
 }
 
-Console.WriteLine(AccermanFunc(firstNum, secondNum));
+
+
+
+Console.WriteLine("Выберите, какую задачу вы хотите посмотреть, 1, 2 или 3?");
+string choice = String.Empty;
+choice = Console.ReadLine()!; 
+
+if (choice == "1")
+{
+    Console.WriteLine("ЗАДАНИЕ: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.");
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    NumListRec(num);
+}
+else if(choice == "2")
+{
+    Console.WriteLine("ЗАДАНИЕ: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.");
+    Console.WriteLine("Введите первое число");
+    int minBorder = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int maxBorder = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(SummRec(minBorder, maxBorder));
+}
+else if(choice == "3")
+{
+    Console.WriteLine("ЗАДАНИЕ: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
+    Console.WriteLine("Введите первое число");
+    int firstNum = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int secondNum = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(AccermanFunc(firstNum, secondNum));
+}
+else Console.WriteLine("Вы ввели неверное число");
